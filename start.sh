4c2d195db428e0c458ac6c7fb0d71001b6f88ae7
zoxibox <djzoxi@gmail.com>
2015-04-09 12:09:22 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -14,10 +14,10 @@ HTTPS_PORT=4050
 CONTEXT_PATH=/
 MAX_MEMORY=200
 MUSIC_FOLDER=/music
+PODCAST_FOLDER=/music/podcasts
+PLAYLIST_FOLDER=/music/playlists
 
 SUBSONIC_USER=nobody
-
-
 
 export LANG=POSIX
 export LC_ALL=en_US.UTF-8
@@ -28,6 +28,8 @@ export LC_ALL=en_US.UTF-8
                   --https-port=$HTTPS_PORT \
                   --max-memory=$MAX_MEMORY \
                   --default-music-folder=$MUSIC_FOLDER \
+		  --default-podcast-folder=$PODCAST_FOLDER \
+                  --default-playlist-folder=$PLAYLIST_FOLDER
 		  
 sleep 5
 tail -f /subsonic/subsonic_sh.log
